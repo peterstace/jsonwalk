e8b385eaab62d24b43b390b1c9b0cd2f13bbd787
Peter Stace <peterstace@gmail.com>
2022-06-23 06:04:06 +1000

    Add initial tests for Window type

---
+++ window_test.go
@@ -0,0 +1,63 @@
+package jsonwindow_test
+
+import (
+	"encoding/hex"
+	"io"
+	"reflect"
+	"strconv"
+	"testing"
+
+	"github.com/peterstace/jsonwindow"
+)
+
+var (
+	startObj = jsonwindow.Token{
+		Type: jsonwindow.OpenObjectToken,
+		Raw:  []byte("{"),
+	}
+	closeObj = jsonwindow.Token{
+		Type: jsonwindow.CloseObjectToken,
+		Raw:  []byte("}"),
+	}
+)
+
+func whitespace(raw string) jsonwindow.Token {
+	return jsonwindow.Token{
+		Type: jsonwindow.WhitespaceToken,
+		Raw:  []byte(raw),
+	}
+}
+
+func TestNextToken(t *testing.T) {
+	for i, tc := range []struct {
+		input string
+		want  []jsonwindow.Token
+	}{
+		{`{}`, []jsonwindow.Token{startObj, closeObj}},
+		{`{ }`, []jsonwindow.Token{startObj, whitespace(" "), closeObj}},
+		{` {}`, []jsonwindow.Token{whitespace(" "), startObj, closeObj}},
+		{`{} `, []jsonwindow.Token{startObj, closeObj, whitespace(" ")}},
+	} {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			t.Log("input:")
+			t.Log(hex.Dump([]byte(tc.input)))
+			win := jsonwindow.New([]byte(tc.input))
+			var got []jsonwindow.Token
+			for {
+				tok, err := win.NextToken()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					t.Fatalf("unexpected error: %v", err)
+				}
+				got = append(got, tok)
+			}
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Logf("got: %v", got)
+				t.Logf("want: %v", tc.want)
+				t.Errorf("mismatch")
+			}
+		})
+	}
+}
