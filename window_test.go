a9fd0aa3baef4c86a913831def246fe69afbfbbe
Peter Stace <peterstace@gmail.com>
2022-06-24 05:50:40 +1000

    Add tests for NextToken

--- window_test.go
+++ window_test.go
@@ -92,7 +92,7 @@ func TestNextToken(t *testing.T) {
 
 			// Make sure the parsed tokens match those expected.
 			got := getAllTokens(t, []byte(tc.input))
-			expectTokenSliceEq(t, got, tc.want)
+			expectDeepEq(t, got, tc.want)
 
 			// Make sure that when whitespace is added between any two tokens
 			// in the stream, that we get the original stream back when
@@ -114,9 +114,50 @@ func TestNextToken(t *testing.T) {
 					logBytes(t, rawWithWS)
 
 					reparsed := getAllTokens(t, rawWithWS)
-					expectTokenSliceEq(t, reparsed, tc.want)
+					expectDeepEq(t, reparsed, tc.want)
 				})
 			}
+		})
+	}
+}
+
+func TestNextValue(t *testing.T) {
+	for i, tc := range []struct {
+		typ jsonwindow.ValueType
+		raw string
+	}{
+		{jsonwindow.NullValue, `null`},
+		{jsonwindow.BooleanValue, `true`},
+		{jsonwindow.BooleanValue, `false`},
+		{jsonwindow.StringValue, `"hello"`},
+		{jsonwindow.NumberValue, `123`},
+		{jsonwindow.ArrayValue, `[123]`},
+		{jsonwindow.ArrayValue, `[123,456]`},
+		{jsonwindow.ArrayValue, `[123,"foo"]`},
+		{jsonwindow.ArrayValue, `[[123]]`},
+		{jsonwindow.ArrayValue, `[{"x":"y"}]`},
+		{jsonwindow.ObjectValue, `{"k":123}`},
+		{jsonwindow.ObjectValue, `{"k":123,"n":456}`},
+		{jsonwindow.ObjectValue, `{"k":123,"n":"foo"}`},
+		{jsonwindow.ObjectValue, `{"k":[123]}`},
+		{jsonwindow.ObjectValue, `{"k":{"x":"y"}}`},
+	} {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			input := []byte("[" + tc.raw + "]")
+			win := jsonwindow.New(input)
+			open, err := win.NextToken()
+			assertNoError(t, err)
+			expectEq(t, jsonwindow.OpenArrayToken, open.Type)
+			got, err := win.NextValue()
+			assertNoError(t, err)
+			want := jsonwindow.Value{
+				Type: tc.typ,
+				Raw:  []byte(tc.raw),
+			}
+			expectDeepEq(t, want, got)
+			cls, err := win.NextToken()
+			assertNoError(t, err)
+			expectEq(t, jsonwindow.CloseArrayToken, cls.Type)
 		})
 	}
 }
@@ -138,19 +179,35 @@ func getAllTokens(t *testing.T, input []byte) []jsonwindow.Token {
 	return all
 }
 
-func expectTokenSliceEq(t *testing.T, got, want []jsonwindow.Token) {
-	t.Helper()
-	if !reflect.DeepEqual(got, want) {
-		t.Logf("got: %v", got)
-		t.Logf("want: %v", want)
-		t.Fatalf("mismatch")
-	}
-}
-
 func logBytes(t *testing.T, byts []byte) {
 	t.Helper()
 	dump := strings.TrimSpace(hex.Dump(byts))
 	for _, line := range strings.Split(dump, "\n") {
 		t.Log(line)
+	}
+}
+
+func assertNoError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatalf("expected no error but got: %v", err)
+	}
+}
+
+func expectDeepEq[T any](t *testing.T, want, got T) {
+	t.Helper()
+	if !reflect.DeepEqual(want, got) {
+		t.Logf("want: %v", want)
+		t.Logf("got: %v", got)
+		t.Errorf("not deep equal")
+	}
+}
+
+func expectEq[T comparable](t *testing.T, want, got T) {
+	t.Helper()
+	if want != got {
+		t.Logf("want: %v", want)
+		t.Logf("got: %v", got)
+		t.Errorf("not equal")
 	}
 }
