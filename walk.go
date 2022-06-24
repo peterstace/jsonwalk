b2ec2476681e7f30c4cfa167da5101b35451cdcd
Peter Stace <peterstace@gmail.com>
2022-06-24 16:57:35 +1000

    Additional hacking on simplification

--- walk.go
+++ walk.go
@@ -44,8 +44,11 @@ func parseValue(raw []byte) (int, error) {
 		n, err := continueArray(raw[1:])
 		return 1 + n, err
 	case '"':
-		panic("not implemented yet")
+		// TODO: continueString rather than parseString
+		str, err := parseString(raw)
+		return len(str), err
 	case 't':
+		// TODO: abstract keywords
 		if len(raw) < len("true") {
 			return 0, io.ErrUnexpectedEOF
 		}
@@ -54,6 +57,7 @@ func parseValue(raw []byte) (int, error) {
 		}
 		return len("true"), nil
 	case 'f':
+		// TODO: abstract keywords
 		if len(raw) < len("false") {
 			return 0, io.ErrUnexpectedEOF
 		}
@@ -62,6 +66,7 @@ func parseValue(raw []byte) (int, error) {
 		}
 		return len("false"), nil
 	case 'n':
+		// TODO: abstract keywords
 		if len(raw) < len("null") {
 			return 0, io.ErrUnexpectedEOF
 		}
@@ -70,8 +75,10 @@ func parseValue(raw []byte) (int, error) {
 		}
 		return len("null"), nil
 	default:
-		if raw[0] >= '0' && raw[0] <= '9' || raw[0] == '-' {
-			panic("not implemented yet")
+		// TODO: could put number start chars in their own case?
+		if isNumberStartChar(raw[0]) {
+			n := continueNumber(raw[1:])
+			return n + 1, nil
 		}
 		return 0, fmt.Errorf("JSON value must start with" +
 			" '{', '[', '\"', 't', 'f', 'n', '-', or a digit")
@@ -92,8 +99,8 @@ func continueObject(raw []byte, fn func(key, val []byte) error) (int, error) {
 	}
 
 	for {
-		// Consume key. It must be a string.
-		i += countWS(raw[i:])
+		// Consume key. It must be a string. Note that whitespace has already
+		// been stripped (either before the loop or at the end of the loop).
 		key, err := parseString(raw[i:])
 		if err != nil {
 			return 0, err
@@ -112,18 +119,18 @@ func continueObject(raw []byte, fn func(key, val []byte) error) (int, error) {
 
 		// Consume the value. It doesn't matter what type of value it is.
 		i += countWS(raw[i:])
-		valLen, err := parseValue(raw[i:])
+		val, err := parseValue(raw[i:])
 		if err != nil {
 			return 0, err
 		}
 
 		// Use callback if provided.
 		if fn != nil {
-			if err := fn(key, raw[i:i+valLen]); err != nil {
+			if err := fn(key, raw[i:i+val]); err != nil {
 				return 0, err
 			}
 		}
-		i += valLen
+		i += val
 
 		// Check to see if we're at the end of the object, of if there are
 		// more key/value pairs.
@@ -133,6 +140,7 @@ func continueObject(raw []byte, fn func(key, val []byte) error) (int, error) {
 		}
 		if raw[i] == ',' {
 			i++
+			i += countWS(raw[i:])
 			continue
 		}
 		if raw[i] == '}' {
@@ -158,7 +166,31 @@ func continueArray(raw []byte) (int, error) {
 	}
 
 	for {
-		// TODO
+		// Consume the value. It doesn't matter what type of value it is. Note:
+		// whitespace has already been stripped (either before the loop, or
+		// before returning to the top of the loop).
+		val, err := parseValue(raw[i:])
+		if err != nil {
+			return 0, err
+		}
+		i += val
+
+		// Check to see if we're at the end of the array, of if there are more
+		// values.
+		i += countWS(raw[:i])
+		if i >= len(raw) {
+			return 0, io.ErrUnexpectedEOF
+		}
+		if raw[i] == ',' {
+			i++
+			i += countWS(raw[i:])
+			continue
+		}
+		if raw[i] == ']' {
+			i++
+			return i, nil
+		}
+		return 0, fmt.Errorf("',' or ']' must come after value in JSON array")
 	}
 }
 
@@ -191,4 +223,28 @@ func parseString(raw []byte) ([]byte, error) {
 			}
 		}
 	}
+}
+
+// assume: first char of number is already consumed
+func continueNumber(raw []byte) int {
+	i := 0
+	for i < len(raw) && isNumberContinueChar(raw[i]) {
+		i++
+	}
+	return i
+}
+
+func isNumberStartChar(c byte) bool {
+	return false ||
+		(c >= '0' && c <= '9') ||
+		c == '-'
+}
+
+func isNumberContinueChar(c byte) bool {
+	return false ||
+		(c >= '0' && c <= '9') ||
+		c == '.' ||
+		c == 'e' ||
+		c == 'E' ||
+		c == '+'
 }
